2a9b1d43d67efd055c412671c3e5f2f622f6f821
zhaowei19920524 <zhaowei0524@outlook.com>
2017-03-15 18:32:23 -0400

    add p1 info

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import colleague3
 import colleague4
 
 colleague.say_hello("zhaow9", "Wei Zhao")
-colleague1.say_hello("", "")
+colleague1.say_hello("abc", "bca")
 colleague2.say_hello("", "")
 colleague3.say_hello("", "")
 colleague4.say_hello("", "")
