bdcee9fa6d638e371eab191df40c25643b5d8cf7
zhaowei19920524 <zhaowei0524@outlook.com>
2017-03-15 18:28:23 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import colleague
+import colleague1
+import colleague2
+import colleague3
+import colleague4
+# import colleague5
+# import colleague6
+# import colleague7
+# import colleague8
+# import colleague9
+
+colleague.say_hello("zhaow9", "Wei Zhao")
+colleague1.say_hello("zhaow9", "Wei Zhao")
+colleague2.say_hello("zhaow9", "Wei Zhao")
+colleague3.say_hello("zhaow9", "Wei Zhao")
+colleague4.say_hello("zhaow9", "Wei Zhao")
