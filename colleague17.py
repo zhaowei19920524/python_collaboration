5513043d8bb96f5cd96636b3d75bf6d80621ebaa
zhaowei19920524 <zhaowei0524@outlook.com>
2017-03-15 23:10:37 -0400

    final code

---
+++ colleague17.py
@@ -0,0 +1,2 @@
+def say_hello(ID, name):
+	print ID + " " + name + ": " + "[what you want to say]"
--- main.py
+++ main.py
@@ -3,6 +3,32 @@ import colleague1
 import colleague2
 import colleague3
 import colleague4
+import colleague5
+import colleague6
+import colleague7
+import colleague8
+import colleague9
+import colleague10
+import colleague11
+import colleague12
+import colleague13
+import colleague14
+import colleague15
+import colleague16
+import colleague17
+import colleague18
+import colleague19
+import colleague20
+import colleague21
+import colleague22
+import colleague23
+import colleague24
+import colleague25
+import colleague26
+import colleague27
+import colleague28
+import colleague29
+import colleague30
 
 colleague.say_hello("zhaow9", "Wei Zhao")
 
@@ -13,3 +39,55 @@ colleague2.say_hello("", "")
 colleague3.say_hello("", "")
 
 colleague4.say_hello("", "")
+
+colleague5.say_hello("", "")
+
+colleague6.say_hello("", "")
+
+colleague7.say_hello("", "")
+
+colleague8.say_hello("", "")
+
+colleague9.say_hello("", "")
+
+colleague10.say_hello("", "")
+
+colleague11.say_hello("", "")
+
+colleague12.say_hello("", "")
+
+colleague13.say_hello("", "")
+
+colleague14.say_hello("", "")
+
+colleague15.say_hello("", "")
+
+colleague16.say_hello("", "")
+
+colleague17.say_hello("", "")
+
+colleague18.say_hello("", "")
+
+colleague19.say_hello("", "")
+
+colleague20.say_hello("", "")
+
+colleague21.say_hello("", "")
+
+colleague22.say_hello("", "")
+
+colleague23.say_hello("", "")
+
+colleague24.say_hello("", "")
+
+colleague25.say_hello("", "")
+
+colleague26.say_hello("", "")
+
+colleague27.say_hello("", "")
+
+colleague28.say_hello("", "")
+
+colleague29.say_hello("", "")
+
+colleague30.say_hello("", "")
